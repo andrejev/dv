d9ba4f5d94ea4c635885a13e00ab2c62c9196b0e
andrejev <andrejev.pavel@gmail.com>
2016-05-02 23:07:06 +0200

    add demo with jLouvain

---
+++ demo/jLouvain/edges.js
@@ -0,0 +1,10 @@
+var edges = [{"sourceId": 15, "targetId": 12}, {"sourceId": 2, "targetId": 1}, {
+  "sourceId": 6,
+  "targetId": 4
+}, {"sourceId": 35, "targetId": 32}, {"sourceId": 28, "targetId": 26}, {"sourceId": 17, "targetId": 15}, {
+  "sourceId": 6,
+  "targetId": 5
+}, {"sourceId": 8, "targetId": 6}, {"sourceId": 22, "targetId": 20}, {"sourceId": 6, "targetId": 0}, {
+  "sourceId": 7,
+  "targetId": 0
+}, {"sourceId": 35, "targetId": 19}, {"sourceId": 22, "targetId": 15}];
